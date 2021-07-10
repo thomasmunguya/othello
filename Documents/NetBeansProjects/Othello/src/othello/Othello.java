cdbed15dbefc4fddaf08c02a857b1f249fbe47b5
Thomas Munguya <thomasmunguya@gmail.com>
2021-07-10 13:06:52 +0200

    Modified Othello.java to load the OthelloViewController class in the scene

--- Documents/NetBeansProjects/Othello/src/othello/Othello.java
+++ Documents/NetBeansProjects/Othello/src/othello/Othello.java
@@ -6,11 +6,7 @@
 package othello;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,23 +17,14 @@ public class Othello extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        OthelloViewController othelloViewController = new OthelloViewController();
+        Scene scene = new Scene(othelloViewController, 1067, 695);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Arzaan's Othello Client");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
